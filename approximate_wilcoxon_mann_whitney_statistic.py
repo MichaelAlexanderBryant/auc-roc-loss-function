e2948c4b9614f823fd6740c824783ac11380e1aa
Michael Bryant <58651380+MichaelABryant@users.noreply.github.com>
2022-08-05 11:53:00 -0700

    Update approximate_wilcoxon_mann_whitney_statistic.py

--- approximate_wilcoxon_mann_whitney_statistic.py
+++ approximate_wilcoxon_mann_whitney_statistic.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def ApproximateWMWStatistic(gamma=0.4, p=2):
+def WMWStatistic(gamma=0.4, p=2):
     
     """Computes a loss function based on the approximation of the normalized
     Wilcoxon-Mann-Whitney (WMW) statistic.
